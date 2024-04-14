3552c118aa25456b24b0c14c5906e00012c37288
Sesaka Aji <sesaka.aji@sprout.co.id>
2024-04-15 02:30:51 +0700

    anjir

--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,39 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
 
 export default function Home() {
     const [isLightOn, setIsLightOn] = useState(false);
     const [isBoxOpened, setIsBoxOpened] = useState(false);
 
     return (
-        <div className={isLightOn ? styles.light : styles.dark}>
-            <Head>
-                <title>Birthday Surprise</title>
-            </Head>
+        <div></div>
+        // <div className={isLightOn ? styles.light : styles.dark}>
+        //     <Head>
+        //         <title>Birthday Surprise</title>
+        //     </Head>
 
-            <button onClick={() => setIsLightOn(!isLightOn)}>
-                {isLightOn ? 'Turn Off' : 'Turn On'} Lights
-                <Image src="/lamp.svg" alt="Lamp" width={24} height={24} />
-            </button>
+        //     <button onClick={() => setIsLightOn(!isLightOn)}>
+        //         {isLightOn ? 'Turn Off' : 'Turn On'} Lights
+        //         <Image src="/lamp.svg" alt="Lamp" width={24} height={24} />
+        //     </button>
 
-            {isLightOn && (
-                <div>
-                    <button onClick={() => setIsBoxOpened(true)}>
-                        Open Surprise Box
-                        <Image src="/box.svg" alt="Box" width={48} height={48} />
-                    </button>
-                </div>
-            )}
+        //     {isLightOn && (
+        //         <div>
+        //             <button onClick={() => setIsBoxOpened(true)}>
+        //                 Open Surprise Box
+        //                 <Image src="/box.svg" alt="Box" width={48} height={48} />
+        //             </button>
+        //         </div>
+        //     )}
 
-            {isBoxOpened && (
-                <div>
-                    <p>Happy Birthday! 🎉</p>
-                    {/* Insert more interactions and animations here */}
-                </div>
-            )}
-        </div>
+        //     {isBoxOpened && (
+        //         <div>
+        //             <p>Happy Birthday! 🎉</p>
+        //             {/* Insert more interactions and animations here */}
+        //         </div>
+        //     )}
+        // </div>
     );
 }
